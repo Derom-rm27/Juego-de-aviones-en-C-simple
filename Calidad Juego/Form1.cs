259eaecbaa23a784c95e1b4541e951c57aac9c43
Derom-rm27 <137957078+Derom-rm27@users.noreply.github.com>
2025-12-12 11:02:23 -0500

    Simplifica audio de fondo y dependencias

--- Calidad Juego/Form1.cs
+++ Calidad Juego/Form1.cs
@@ -1,10 +1,15 @@
+using System.Media;
 using Calidad_Juego.Escenarios;
-        private NaveFiguraEscalada? navePrincipalFigura;
 
 
-            navePrincipalFigura ??= NaveFactory.ObtenerNavePrincipal();
+        private readonly SoundPlayer reproductorMusica = new();
             CrearNave(navex, 0, NaveFactory.TipoNavePrincipal, Color.SeaGreen, vidaJugador, navePrincipalFigura);
             return EscenarioEspacial.CrearFondo(tamano, GeneradorAleatorio);
         private void CrearNave(PictureBox avion, int anguloRotacion, int tipo, Color color, int vida, NaveFiguraEscalada? figuraPersonalizada = null)
             var figuraEscalada = figuraPersonalizada ?? plantilla.Escalar();
-}
+}                reproductorMusica.Stop();
+            const string rutaCancion = @"C:\\Users\\derom\\Music\\Eagle Flight (OST)  Inon Zur - The Grim Falcon.wav";
+                    reproductorMusica.PlayLooping();
+                reproductorMusica.SoundLocation = rutaCancion;
+                reproductorMusica.Load();
+                reproductorMusica.PlayLooping();
